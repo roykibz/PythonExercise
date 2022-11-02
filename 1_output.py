a80f7bfc73103a5f93e91f677d5805ec59eda82f
roykibz <roykibz79@gmail.com>
2022-11-02 14:08:21 +0300

    First Commit

---
+++ 1_output.py
@@ -0,0 +1,8 @@
+# Output in python
+# The Print () function is used to display something on the screen
+print("Hello World")
+
+# Python statements have no terminator
+print(2+2)
+
+print(10 % 3)
---
+++ 2_datatypes.py
@@ -0,0 +1,29 @@
+# Datatypes in python
+
+# Primitve datatypes
+
+x = 5  # Int data type
+y = 2.5  # Float data type
+
+# Python allows variable declarations on the same line
+# x, y = 5, 2.5
+
+# Python allows setting multiple variables to the same value
+a = b = c = 3
+
+string = ("Hello There")  # Str data type
+is_true = True  # Boolean data type
+is_false = False  # Boolean data type
+my_first_list = [1, 2, 3]  # List data type
+none_type = None
+
+my_char = ('a')
+
+print(type(x))
+print(type(y))
+print(type(a))
+print(type(string))
+print(type(is_true))
+print(type(my_first_list))
+print(type(none_type))
+print(type(my_char))
